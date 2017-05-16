f528e98238c9a9c277fe27035277fd2e10a2eb87
Yorley <yorleymaria@outlook.com>
2017-05-16 00:14:17 -0600

    if_struct , decl_assig, condition, expr

--- New folder/result.asm
+++ New folder/result.asm
@@ -4094,3 +4094,819 @@ SECTION .txt
 global _start
 
 _start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
+SECTION .data
+
+SEECTION .bss
+
+SECTION .txt
+global _start
+
+_start:
